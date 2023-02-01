a6fe38acb8facafc0bb4aa25182c8914c277d954
gabriel018 <duke.comandos@gmail.com>
2023-02-01 18:04:28 -0300

    1

--- Personal_Style/Personal_Style/Program.cs
+++ Personal_Style/Personal_Style/Program.cs
@@ -18,6 +18,9 @@ namespace Personal_Style
                 sqlConnection = new SqlConnection(conex);
                 sqlConnection.Open();
                 Console.WriteLine("Conectado com sucesso");
+                
+                Console.WriteLine("digite o id");
+                 int idd = int.Parse(Console.ReadLine());
                 Console.WriteLine("Digite seu nome");
                 string nome_add = Console.ReadLine();
                 Console.WriteLine("Digite seu cpf");
@@ -25,15 +28,17 @@ namespace Personal_Style
                 Console.WriteLine("Digite o telefone");
                 int telefone_add = int.Parse(Console.ReadLine());
 
-                string add_diretor = "INSERT INTO diretor(nome,cpf,telefone) VALUES(" + nome_add + "," + cpf_add + "," + telefone_add + ")";
+
+                string add_diretor = "INSERT INTO diretor(id,nome,cpf,telefone) VALUES('" + idd + "','" + nome_add + "','" + cpf_add + "','" + telefone_add + "')";
                 SqlCommand salvar_dados = new SqlCommand(add_diretor, sqlConnection);
+                salvar_dados.ExecuteNonQuery();
                 Console.WriteLine("Dados salvos ");
             }
             catch(Exception e)
             {
-                Console.WriteLine("Falha no Banco de Dados",(e.Message));
+                Console.WriteLine("Dados nao foram salvos",(e.Message));
 
-                Console.WriteLine("");
+
             }
 
 
