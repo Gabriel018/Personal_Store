92f8a09f9bee94e5fc80fab1adbe788a8e50003f
gabriel018 <duke.comandos@gmail.com>
2023-02-01 21:49:53 -0300

    11

---
+++ Personal_DBA.sql
@@ -0,0 +1,7 @@
+USE [master]
+GO
+
+/****** Object:  Database [Personal_Style]    Script Date: 01/02/2023 15:37:24 ******/
+DROP DATABASE [Personal_Style]
+GO
+
---
+++ Personal_Style/Personal_Style/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Personal_Style
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlConnection;
+            string conex = @"Data Source=DUKE\SQLEXPRESS;Integrated Security=True";
+            try
+            {
+                sqlConnection = new SqlConnection(conex);
+                sqlConnection.Open();
+                Console.WriteLine("Conectado com sucesso");
+                
+               
+                Console.WriteLine("Digite seu nome");
+                string nome_add = Console.ReadLine();
+                Console.WriteLine("Digite seu cpf");
+                int cpf_add = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o telefone");
+                int telefone_add = int.Parse(Console.ReadLine());
+
+
+                string add_diretor = "INSERT INTO diretor(nome,cpf,telefone) " +
+                    "VALUES('" + nome_add + "','" + cpf_add + "','" + telefone_add + "')";
+                SqlCommand salvar_dados = new SqlCommand(add_diretor, sqlConnection);
+                salvar_dados.ExecuteNonQuery();
+                Console.WriteLine("Dados salvos ");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Dados nao foram salvos",(e.Message));
+
+
+            }
+
+
+            
+        }
+    }
+}
+
